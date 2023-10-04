6cdf44cb5b033fd5140dbcb653178c764f453e7b
TerryFung <terryyessfung@gmail.com>
2023-10-04 14:03:26 +0800

    update

--- bin/rss.sh
+++ bin/rss.sh
@@ -7,12 +7,17 @@ declare -a rssList=(
 'LinusTechTips - https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw'
 )
 
-rsstail -u https://hnrss.org/newest -n 3 -N -Z '-' >| /tmp/rss/hackerNews 2>/dev/null &
+if [ ! -d "/tmp/rss" ]
+then
+    mkdir /tmp/rss
+fi
+
+rsstail -u 'https://hnrss.org/newest' -n 3 -N -Z '-' > /tmp/rss/hackerNews 2>/dev/null &
 
 for rss in "${rssList[@]}"; do
 	name=$(printf '%s\n' "$rss" |awk '{print $1}')
 	url=$(printf '%s\n' "$rss" |awk '{print $NF}')
-	rsstail -u $url -n 1 -N -Z "$name - " >| /tmp/rss/$name 2>/dev/null &
+	rsstail -u $url -n 1 -N -Z "$name - " > /tmp/rss/$name 2>/dev/null &
 done
 
 
